d9289a45d00c8ecae6ac32504083f3b516836766
xclin <729885579@qq.com>
2016-08-30 23:38:32 +0800

    Commit AI

--- js/script.js
+++ js/script.js
@@ -1,11 +1,76 @@
 var canvas = document.getElementById('board');
 var context = canvas.getContext('2d');
+var me = true;
+//游戏结束标志
+var over = false;
+//棋盘二维数组
+var board = [];
+for (var i = 0; i < 15; i++) {
+	board[i] = [];
+	for (var j = 0; j < 15; j++) {
+		board[i][j] = 0;
+	};
+};
+//赢法三维数组
+var wins = [];
+for (var i = 0; i < 15; i++) {
+	wins[i] = [];
+	for (var j = 0; j < 15; j++) {
+		wins[i][j] = [];
+	};
+};
+/**
+	统计所有的赢法
+*/
+var count = 0;
+for (var i = 0; i < 15; i++) {
+	for (var j = 0; j < 11; j++) {
+		for (var k = 0; k < 5; k++) {
+			wins[i][j+k][count] = true;
+		};
+		count++;
+	};
+};
+
+for (var i = 0; i < 15; i++) {
+	for (var j = 0; j < 11; j++) {
+		for (var k = 0; k < 5; k++) {
+			wins[j+k][i][count] = true;
+		};
+		count++;
+	};
+};
+
+for (var i = 0; i < 11; i++) {
+	for (var j = 0; j < 11; j++) {
+		for (var k = 0; k < 5; k++) {
+			wins[i+k][j+k][count] = true;
+		};
+		count++;
+	};
+};
+
+for (var i = 0; i < 11; i++) {
+	for (var j = 14; j > 3; j--) {
+		for (var k = 0; k < 5; k++) {
+			wins[i+k][j-k][count] = true;
+		};
+		count++;
+	};
+};
+
+//赢法数组统计数组
+var myWin = [];
+var computerWin = [];
+for (var i = 0; i < count; i++) {
+	myWin[i] = 0;
+	computerWin[i] = 0;
+};
+
 
 context.strokeStyle = '#BFBFBF';
 window.onload = function(){
 	drawBoard();
-	onStep(0, 0, true);
-	onStep(1, 1, false);
 }
 
 /**
@@ -41,4 +106,115 @@ var onStep = function(i, j, me) {
 
 	context.fillStyle = gradient;
 	context.fill();
+}
+
+canvas.onclick = function(event) {
+	if (over) {
+		return;
+	};
+	if (!me) {
+		return;
+	};
+	var x = event.offsetX;
+	var y = event.offsetY;
+	var i = Math.floor(x / 30);
+	var j = Math.floor(y / 30);
+	if(board[i][j] == 0){
+		onStep(i, j, me);
+		board[i][j] = 1;
+		for (var k = 0; k < count; k++) {
+			if(wins[i][j][k]){
+				myWin[k]++;
+				computerWin[k] = 6;
+				if(myWin[k] == 5){
+					alert("你赢了！");
+					over = true;
+				}
+			}
+		};
+		if (!over) {
+			me = !me;
+			computerAI();
+		};
+	}
+	
+}
+
+var computerAI = function(){
+	var myScore = [];
+	var computerScore = [];
+	var max = 0;
+	//保存分数最高点的坐标
+	var u = 0, v = 0;
+	for (var i = 0; i < 15; i++) {
+		myScore[i] = [];
+		computerScore[i] = [];
+		for (var j = 0; j < 15; j++) {
+			myScore[i][j] = 0;
+			computerScore[i][j] = 0;
+		};
+	};
+	for (var i = 0; i < 15; i++) {
+		for (var j = 0; j < 15; j++) {
+			if(board[i][j] == 0){
+				for (var k = 0; k < count; k++) {
+					if(wins[i][j][k]){
+						if (myWin[k] == 1) {
+							myScore[i][j] += 200;
+						}else if(myWin[k] == 2){
+							myScore[i][j] += 400;
+						}else if(myWin[k] == 3){
+							myScore[i][j] += 2000;
+						}else if(myWin[k] == 4){
+							myScore[i][j] += 10000;
+						}
+						if(computerWin[k] == 1){
+							computerScore[i][j] += 220;
+						}else if(computerWin[k] == 2){
+							computerScore[i][j] += 420;
+						}else if(computerWin[k] == 3){
+							computerScore[i][j] += 2100;
+						}else if(computerWin[k] == 4){
+							computerScore[i][j] += 20000;
+						}
+					}
+				};
+				if(myScore[i][j] > max){
+					max = myScore[i][j];
+					u = i;
+					v = j;
+				}else if(myScore[i][j] == max){
+					if(computerScore[i][j] > computerScore[u][v]){
+						u = i;
+						v = j;
+					}
+				}
+				if(computerScore[i][j] > max){
+					max = computerScore[i][j];
+					u = i;
+					v = j;
+				}else if(computerScore[i][j] == max){
+					if(myScore[i][j] > myScore[u][v]){
+						u = i;
+						v = j;
+					}
+				}
+			}
+		};
+	};
+	onStep(u, v, false);
+	board[u][v] = 2;
+	for (var k = 0; k < count; k++) {
+			if(wins[u][v][k]){
+				computerWin[k]++;
+				myWin[k] = 6;
+				if(computerWin[k] == 5){
+					alert("你输了！");
+					over = true;
+				}
+			}
+		};
+		if (!over) {
+			me = !me;
+		};
 }
